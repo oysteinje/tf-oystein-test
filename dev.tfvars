372beea7bc451a72f3c5723691cec9367ca52518
oystein.straete <oystein.straete@klp.no>
2022-06-09 16:25:15 +0200

    upd vars

--- dev.tfvars
+++ dev.tfvars
@@ -1,6 +1,3 @@
 resource_group_name = "rg-oystein-test"
 location = "WestEurope" 
 environment = "dev"
-ARM_CLIENT_ID = "c376f14f-2994-4ed9-8df1-9e3d1087d749"
-ARM_TENANT_ID = "040886fa-09b0-4e7f-8cad-9e9797dd96a0"
-ARM_SUBSCRIPTION_ID = "a83145a3-215b-44a4-9387-a540faaa58e9"
--- variables.tf
+++ variables.tf
@@ -21,21 +21,3 @@ variable "environment" {
     description = "Environment for the resources."
     default     = "dev"
 }
-
-variable "ARM_CLIENT_ID" {
-  type = string
-  description = "Client ID for Azure deployments."
-  default = "c376f14f-2994-4ed9-8df1-9e3d1087d749"
-}
-
-variable "ARM_TENANT_ID" {
-  type = string
-  description = "The ID of the tenant to deploy to."
-  default = "040886fa-09b0-4e7f-8cad-9e9797dd96a0"
-}
-
-variable "ARM_SUBSCRIPTION_ID" {
-  type = string
-  description = "The ID of the subscription to deploy to."
-  default = "a83145a3-215b-44a4-9387-a540faaa58e9"
-}
